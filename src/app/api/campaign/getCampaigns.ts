85d00b4a07b83d4de324b9c644f6ee548338b314
Nicolás Fainstein <nicofains1@gmail.com>
2024-02-25 00:37:48 -0300

    allowed tokens

--- src/app/api/campaign/getCampaigns.ts
+++ src/app/api/campaign/getCampaigns.ts
@@ -1,25 +1,59 @@
 import { sql } from "@vercel/postgres";
-import { CampaignsRow } from "../../../types/db";
+import { CampaignAllowedTokensRow, CampaignsRow } from "../../../types/db";
 import { StoredCampaign } from "@/types/campaign";
 import { transformCampaigns } from "../transform/campaign";
+import { getCampaignAvailableTokens } from "./getCampaignAvailableTokens";
+import { getUser } from "../user/getUser";
+
+const getCampaignExtraData = async (
+  campaignId: number,
+  userId: number
+): Promise<{
+  allowedTokens: CampaignAllowedTokensRow[];
+  ownerAddress: string;
+}> => {
+  const allowedTokens = await getCampaignAvailableTokens(campaignId);
+  const user = await getUser(userId);
+  return { allowedTokens, ownerAddress: user.address };
+};
 
 export async function getOpenCampaigns(start = 0): Promise<StoredCampaign[]> {
-  const { rows } =
+  const { rows: campaigns } =
     await sql<CampaignsRow>`SELECT campaign_id, name, cafe_crypto_unit, goal_cc, end_date, total_received, user_id FROM campaigns WHERE is_open=true ORDER BY created_at LIMIT 20 OFFSET ${start};`;
 
-  return transformCampaigns(rows);
+  return transformCampaigns({
+    campaigns,
+    allowedTokens: [],
+    ownerAddress: "0x",
+  });
 }
 
 export async function getCampaign(campaignId: number): Promise<StoredCampaign> {
-  const { rows } =
+  const { rows: campaigns } =
     await sql<CampaignsRow>`SELECT * FROM campaigns WHERE campaign_id=${campaignId};`;
-  return transformCampaigns(rows)[0];
+
+  const { campaign_id, user_id } = campaigns[0];
+  const { allowedTokens, ownerAddress } = await getCampaignExtraData(
+    campaign_id,
+    user_id
+  );
+
+  return transformCampaigns({
+    campaigns,
+    allowedTokens,
+    ownerAddress,
+  })[0];
 }
 
 export async function getUserCampaigns(
   userId: number
 ): Promise<StoredCampaign[]> {
-  const { rows } =
+  const { rows: campaigns } =
     await sql<CampaignsRow>`SELECT * from campaigns WHERE user_id=${userId};`;
-  return transformCampaigns(rows);
+
+  return transformCampaigns({
+    campaigns,
+    allowedTokens: [],
+    ownerAddress: "0x",
+  });
 }
--- src/components/network-selector/network-selector.tsx
+++ src/components/network-selector/network-selector.tsx
@@ -6,17 +6,29 @@ import { networkList } from "@/constants/networks";
 interface NetworkSelectorProps {
   value: Network;
   setValue: React.Dispatch<React.SetStateAction<Network>>;
+  onChange: (chainId: number) => void;
+  allowedChains: number[];
 }
 
-const NetworkSelector = ({ value, setValue }: NetworkSelectorProps) => {
+const NetworkSelector = ({
+  value,
+  setValue,
+  onChange,
+  allowedChains,
+}: NetworkSelectorProps) => {
   const handleChange = (chainId: string) => {
     const selectedChain = Object.values(networkList).find(
       (network) => network.chainId === +chainId
     );
     if (selectedChain) {
       setValue(selectedChain);
+      onChange(selectedChain.chainId);
     }
   };
+
+  const filteredChains = Object.values(networkList).filter((network) =>
+    allowedChains.includes(network.chainId)
+  );
 
   return (
     <FormControl variant="outlined">
@@ -28,7 +40,7 @@ const NetworkSelector = ({ value, setValue }: NetworkSelectorProps) => {
         onChange={(e) => handleChange(e.target.value)}
         label="Network"
       >
-        {Object.values(networkList).map((network) => (
+        {filteredChains.map((network) => (
           <MenuItem key={network.chainId} value={network.chainId}>
             {network.name}
           </MenuItem>
